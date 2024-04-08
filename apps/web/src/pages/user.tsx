a0046a7cf1912e3b880009743be04122ef575970
Krystian Sz <sztadii@gmail.com>
2024-04-08 12:14:21 +0700

    Add delete button to user page

--- apps/web/src/pages/user.tsx
+++ apps/web/src/pages/user.tsx
@@ -1,26 +1,74 @@
-import { Box, Button, Divider, Skeleton, useDisclosure } from '@chakra-ui/react'
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Skeleton,
+  useDisclosure
+} from '@chakra-ui/react'
 
 import { CreateUpdateUserDrawer } from 'src/components-connected/create-update-user-drawer'
+import { DeleteUserConfirmation } from 'src/components-connected/delete-user-confirmation'
 import { ViewReminderSection } from 'src/components-connected/view-reminder-section'
 import { ViewUserSection } from 'src/components-connected/view-user-section'
+import { useNavigate } from 'src/hooks/use-navigate'
 import { useUserFromUrl } from 'src/hooks/use-user-from-url'
+import { routerPaths } from 'src/router'
 
 export function User() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const {
+    isOpen: isOpenUpdateDrawer,
+    onOpen: onOpenUpdateDrawer,
+    onClose: onCloseUpdateDrawer
+  } = useDisclosure()
+  const {
+    isOpen: isOpenDeleteModal,
+    onOpen: onOpenDeleteModal,
+    onClose: onCloseDeleteModal
+  } = useDisclosure()
   const { data: user, isLoading } = useUserFromUrl()
+  const navigate = useNavigate()
 
   return (
     <>
       {isLoading ? (
-        <Skeleton display="inline-block">
-          <Button>Update user</Button>
-        </Skeleton>
+        <Flex gap={4}>
+          <Skeleton display="inline-block">
+            <Button>Update user</Button>
+          </Skeleton>
+
+          <Skeleton display="inline-block">
+            <Button>Delete user</Button>
+          </Skeleton>
+        </Flex>
       ) : (
-        <Button isDisabled={!user} onClick={onOpen}>
-          Update user
-        </Button>
+        <Flex gap={4}>
+          <Button isDisabled={!user} onClick={onOpenUpdateDrawer}>
+            Update user
+          </Button>
+
+          <Button isDisabled={!user} onClick={onOpenDeleteModal}>
+            Delete user
+          </Button>
+        </Flex>
       )}
-      <CreateUpdateUserDrawer user={user} isOpen={isOpen} onClose={onClose} />
+      <CreateUpdateUserDrawer
+        user={user}
+        isOpen={isOpenUpdateDrawer}
+        onClose={onCloseUpdateDrawer}
+      />
+      <DeleteUserConfirmation
+        user={user}
+        isOpen={isOpenDeleteModal}
+        onClose={() => {
+          onCloseDeleteModal()
+
+          // TODO based on the user role we should redirect somewhere
+          // Admin should be redirected to routerPaths.users.path
+          // Other users should be redirected to login page
+          navigate(routerPaths.users.path)
+        }}
+      />
       <Divider my={4} />
       <ViewUserSection />
       <Box mt={4}>
