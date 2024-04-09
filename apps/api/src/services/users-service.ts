6745f571a2cd01748ac6ed0fd062eaf7773014c4
Krystian Sz <sztadii@gmail.com>
2024-04-09 09:31:45 +0700

    Fix error handling in users-service

--- apps/api/src/services/users-service.ts
+++ apps/api/src/services/users-service.ts
@@ -8,15 +8,23 @@ export class UsersService extends DatabaseService<UserDatabaseRecord> {
   }
 
   async findAll(): Promise<UserResponse[]> {
-    const records = await this.collection.find().toArray()
+    try {
+      const records = await this.collection.find().toArray()
 
-    return records.map(this.mapRecordToResponse)
+      return records.map(this.mapRecordToResponse)
+    } catch {
+      throw new Error('Something went wrong during fetching users')
+    }
   }
 
   async getById(id: UserResponse['id']): Promise<UserResponse> {
-    const record = await this.collection.findOne({
-      _id: new DatabaseId(id)
-    })
+    let record
+
+    try {
+      record = await this.collection.findOne({
+        _id: new DatabaseId(id)
+      })
+    } catch {}
 
     if (!record) {
       throw new Error(`User with ${id} not found!`)
@@ -26,45 +34,57 @@ export class UsersService extends DatabaseService<UserDatabaseRecord> {
   }
 
   async create(userCreateRequest: UserCreateRequest): Promise<UserResponse> {
-    const insertedUser = await this.collection.insertOne({
-      _id: new DatabaseId(),
-      createdAt: new Date(),
-      userOrOrganizationName: userCreateRequest.userOrOrganizationName,
-      role: userCreateRequest.role,
-      email: userCreateRequest.email,
-      githubAccessToken: userCreateRequest.githubAccessToken,
-      headBranch: userCreateRequest.headBranch,
-      baseBranch: userCreateRequest.baseBranch,
-      isOrganization: userCreateRequest.isOrganization
-    })
+    try {
+      const insertedUser = await this.collection.insertOne({
+        _id: new DatabaseId(),
+        createdAt: new Date(),
+        userOrOrganizationName: userCreateRequest.userOrOrganizationName,
+        role: userCreateRequest.role,
+        email: userCreateRequest.email,
+        githubAccessToken: userCreateRequest.githubAccessToken,
+        headBranch: userCreateRequest.headBranch,
+        baseBranch: userCreateRequest.baseBranch,
+        isOrganization: userCreateRequest.isOrganization
+      })
 
-    return this.getById(insertedUser.insertedId.toString())
+      return this.getById(insertedUser.insertedId.toString())
+    } catch (e) {
+      throw new Error('Something went wrong during user create')
+    }
   }
 
   async update(userUpdateRequest: UserUpdateRequest): Promise<UserResponse> {
-    const user = await this.getById(userUpdateRequest.id)
+    try {
+      const user = await this.getById(userUpdateRequest.id)
 
-    await this.collection.updateOne(
-      { _id: new DatabaseId(userUpdateRequest.id) },
-      {
-        $set: {
-          updatedAt: new Date(),
-          userOrOrganizationName: userUpdateRequest.userOrOrganizationName,
-          email: userUpdateRequest.email,
-          role: userUpdateRequest.role,
-          githubAccessToken: userUpdateRequest.githubAccessToken,
-          headBranch: userUpdateRequest.headBranch,
-          baseBranch: userUpdateRequest.baseBranch,
-          isOrganization: userUpdateRequest.isOrganization
+      await this.collection.updateOne(
+        { _id: new DatabaseId(userUpdateRequest.id) },
+        {
+          $set: {
+            updatedAt: new Date(),
+            userOrOrganizationName: userUpdateRequest.userOrOrganizationName,
+            email: userUpdateRequest.email,
+            role: userUpdateRequest.role,
+            githubAccessToken: userUpdateRequest.githubAccessToken,
+            headBranch: userUpdateRequest.headBranch,
+            baseBranch: userUpdateRequest.baseBranch,
+            isOrganization: userUpdateRequest.isOrganization
+          }
         }
-      }
-    )
+      )
 
-    return this.getById(user.id)
+      return this.getById(user.id)
+    } catch {
+      throw new Error('Something went wrong during user update')
+    }
   }
 
   async deleteById(id: UserResponse['id']): Promise<void> {
-    await this.collection.deleteOne({ _id: new DatabaseId(id) })
+    try {
+      await this.collection.deleteOne({ _id: new DatabaseId(id) })
+    } catch {
+      throw new Error('Something went wrong during user delete')
+    }
   }
 
   protected mapRecordToResponse(user: UserDatabaseRecord): UserResponse {
