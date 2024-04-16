01f1a60a6ab4c73ef23e6f25a2bf7df944e4af9b
Krystian Sz <sztadii@gmail.com>
2024-04-16 09:59:33 +0700

    Validate env variables with ZOD

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -1,12 +1,27 @@
-export const config = {
+import * as z from 'zod'
+
+const configSchema = z.object({
+  github: z.object({
+    authClientId: z.string(),
+    authClientSecret: z.string(),
+    appId: z.string(),
+    appPrivateKey: z.string()
+  }),
+  email: z.object({
+    user: z.string(),
+    password: z.string()
+  })
+})
+
+export const config = configSchema.parse({
   github: {
-    authClientId: process.env.GITHUB_AUTH_CLIENT_ID!,
-    authClientSecret: process.env.GITHUB_AUTH_CLIENT_SECRET!,
-    appId: process.env.GITHUB_APP_ID!,
-    appPrivateKey: process.env.GITHUB_APP_PRIVATE_KEY!
+    authClientId: process.env.GITHUB_AUTH_CLIENT_ID,
+    authClientSecret: process.env.GITHUB_AUTH_CLIENT_SECRET,
+    appId: process.env.GITHUB_APP_ID,
+    appPrivateKey: process.env.GITHUB_APP_PRIVATE_KEY
   },
   email: {
-    user: process.env.EMAIL_USER!,
-    password: process.env.EMAIL_PASSWORD!
+    user: process.env.EMAIL_USER,
+    password: process.env.EMAIL_PASSWORD
   }
-}
+})
