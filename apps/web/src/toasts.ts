7419abdf5feb463fbc58741fb962c49dfad513b3
Krystian Sz <sztadii@gmail.com>
2024-04-17 11:10:40 +0700

    Refactor toast options

--- apps/web/src/toasts.ts
+++ apps/web/src/toasts.ts
@@ -2,23 +2,28 @@ import { createStandaloneToast } from '@chakra-ui/react'
 
 const { ToastContainer, toast } = createStandaloneToast()
 
-export function showErrorToast(text: string, durationInMilliseconds?: number) {
+export function showErrorToast(text: string, options?: ToastOptions) {
   toast({
     status: 'error',
     title: 'Error',
     description: text,
     position: 'top-right',
-    duration: durationInMilliseconds
+    duration: options?.durationInMilliseconds || 3_000
   })
 }
 
-export function showSuccessToast(text: string) {
+export function showSuccessToast(text: string, options?: ToastOptions) {
   toast({
     status: 'success',
     title: 'Success',
     description: text,
-    position: 'top-right'
+    position: 'top-right',
+    duration: options?.durationInMilliseconds || 3_000
   })
+}
+
+type ToastOptions = {
+  durationInMilliseconds: number
 }
 
 export const ToastProvider = ToastContainer
