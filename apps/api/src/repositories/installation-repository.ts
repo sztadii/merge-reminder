4e0bcca9429f789198c7cd13f27daaf60606cb14
Krystian Sz <sztadii@gmail.com>
2024-04-17 13:04:28 +0700

    Add missing return types

--- apps/api/src/repositories/installation-repository.ts
+++ apps/api/src/repositories/installation-repository.ts
@@ -4,13 +4,16 @@ import { UsersRepository } from './users-repository'
 export class InstallationRepository {
   constructor(private usersRepository: UsersRepository) {}
 
-  async connectRepositories(userId: string, installationId: number) {
+  async connectRepositories(
+    userId: string,
+    installationId: number
+  ): Promise<void> {
     await this.usersRepository.updateById(userId, {
       githubAppInstallationId: installationId
     })
   }
 
-  async disconnectRepositories(userId: string) {
+  async disconnectRepositories(userId: string): Promise<void> {
     const user = await this.usersRepository.getById(userId)
 
     if (user?.githubAppInstallationId) {
