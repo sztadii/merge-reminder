2642fa938b493edbd4b8cf5ba1f9d9ef93d6bc26
Krystian Sz <sztadii@gmail.com>
2024-04-25 20:06:28 +0700

    Make confirmation centered

--- apps/web/src/components/confirmation.tsx
+++ apps/web/src/components/confirmation.tsx
@@ -32,7 +32,7 @@ export function Confirmation({
   confirmButton
 }: ConfirmationProps) {
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader>{title}</ModalHeader>
