86ed554492d3c0cd87ad10feadb33df490dcff00
Krystian Sz <sztadii@gmail.com>
2025-01-22 22:34:58 +0100

    Add simple description to landing

--- apps/web/src/pages/landing.tsx
+++ apps/web/src/pages/landing.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Heading } from '@chakra-ui/react'
+import { Box, Button, Container, Flex, Heading } from '@chakra-ui/react'
 import { Link } from 'wouter'
 
 import { Text } from '@apps/web/components/text'
@@ -13,19 +13,35 @@ export function Landing() {
       justifyContent="center"
       flexDirection="column"
     >
-      <Box>
-        <Text color="gray.500">Welcome to</Text>
-      </Box>
+      <Container>
+        <Box textAlign="center">
+          <Text color="gray.500">Welcome to</Text>
 
-      <Heading size="md" color="gray.500" mb={4}>
-        Merge Reminder
-      </Heading>
+          <Heading size="md" color="gray.500" mb={4}>
+            Merge Reminder
+          </Heading>
+        </Box>
 
-      <Box position="absolute" right={4} top={4}>
-        <Button as={Link} to={routerPaths.dashboard.path}>
-          Dashboard
-        </Button>
-      </Box>
+        <Text color="gray.500" mb={4}>
+          The app is designed to solve a very common problem during development.{' '}
+          <Text color="white">If you use GitFlow</Text> during development, you
+          typically work with two branches: master and develop. The develop
+          branch is used for ongoing development, while the master branch is
+          used for deploying to production.When a release is ready, the develop
+          branch is merged into master. If any issues arise in production, they
+          are usually fixed directly there and then merged back into develop.{' '}
+          <Text color="white">If fixes are not merged back into develop</Text>,
+          it can lead to <Text color="white">merge conflicts</Text>, which are
+          time-consuming. Time equals money, and that's why this project aims to
+          streamline the development process.
+        </Text>
+
+        <Box position="absolute" right={4} top={4}>
+          <Button as={Link} to={routerPaths.dashboard.path}>
+            Dashboard
+          </Button>
+        </Box>
+      </Container>
     </Flex>
   )
 }
