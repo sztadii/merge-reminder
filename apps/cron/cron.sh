0da66711505f2cb0a406af622c53e83f0f432eec
Krystian Sz <sztadii@gmail.com>
2025-01-22 21:55:03 +0100

    Add --fail flag for cron job

--- apps/cron/cron.sh
+++ apps/cron/cron.sh
@@ -1,2 +1,2 @@
-curl --request POST https://merge-reminder-api.onrender.com/trpc/public.sendWarningsForAllUsers \
+curl --fail --request POST https://merge-reminder-api.onrender.com/trpc/public.sendWarningsForAllUsers \
 --header "api-key: ${APP_API_KEY_FOR_PUBLIC_ENDPOINTS}"
